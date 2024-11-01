37d9b8a200687c78f96732784bdd1bc988468f43
DaiMika <maxdietunnie@gmail.com>
2024-11-01 16:42:11 -0700

    update

---
+++ MXHuB/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Course, Module, Quiz
+
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, "course_list.html", {"courses": courses})
+
+def course_detail(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    return render(request, "course_detail.html", {"course": course})
+
+def module_detail(request, module_id):
+    module = get_object_or_404(Module, id=module_id)
+    return render(request, "module_detail.html", {"module": module})
+

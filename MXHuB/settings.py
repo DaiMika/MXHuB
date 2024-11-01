1acf217051a2d2773a1f3dc48ae3cd09c1f29784
DaiMika <maxdietunnie@gmail.com>
2024-11-01 16:57:36 -0700

    update

--- MXHuB/settings.py
+++ MXHuB/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -48,11 +49,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mxhub.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  
+        conn_max_age=600,  
+        ssl_require=True  
+    )
 }
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "mxhub/static"]
